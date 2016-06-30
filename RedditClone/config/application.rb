6fdfbbf47a7173245a00913b23545d867b6a0023
Joy Jing <joy.jing1@gmail.com>
2016-06-30 16:02:03 -0700

    Update to fix authorship

--- RedditClone/config/application.rb
+++ RedditClone/config/application.rb
@@ -1,22 +1,31 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails"
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
-module RedditClone
+module RedditOnRails
   class Application < Rails::Application
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -30,6 +39,6 @@ module RedditClone
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
